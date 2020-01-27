6ca49987139a139a9ce9edd4b49ad3136e736b07
toddmurphy <toddmurphy93@gmail.com>
2020-01-27 18:03:52 -0400

    added seed data

---
+++ users/userModel.js
@@ -0,0 +1,16 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+  getUsers,
+  userRegister,
+  userLogin
+};
+
+//get users
+function getUsers() {}
+
+//post --> register
+function userRegister() {}
+
+//post --> login
+function userLogin() {}
