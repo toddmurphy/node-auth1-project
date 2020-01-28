1a4a76bded350e0608547aa544a2ec6af18d2a27
toddmurphy <toddmurphy93@gmail.com>
2020-01-28 17:01:23 -0400

    working on login and logout

--- api/authRouter.js
+++ api/authRouter.js
@@ -4,7 +4,7 @@ const Users = require('../users/userModel');
 
 const router = express.Router();
 
-//post --> /api/register -->creates a user
+//post --> /api/auth/register -->creates a user
 router.post('/register', (req, res) => {
   const newUser = req.body;
 
@@ -21,16 +21,18 @@ router.post('/register', (req, res) => {
     });
 });
 
-//post --> /api/login
+//post --> /api/auth/login
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy(username)
     .first()
     .then(user => {
-      //    login works without compareSync --> user && bc.compareSync(password, user.password
+      //    login works without compareSync --> user && bc.compareSync(password, user.password --> //check the password is valid --> if they match it works
       if (user && bc.compareSync(password, user.password)) {
-        //check the password is valid --> if they match it works
+        req.session.loggedIn = true;
+        req.session.userID = user.id;
+
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid credentials' });
@@ -40,6 +42,21 @@ router.post('/login', (req, res) => {
       console.log(error);
       res.status(500).json({ message: 'Sorry, no login working on the server', error });
     });
+});
+
+//get --> /api/auth/logout
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({ message: 'You can checkout anytime you want!' });
+      } else {
+        res.status(200).json({ message: 'Goodbye, thanks for using our app!' });
+      }
+    });
+  } else {
+    res.status(204).json({ message: 'Goodbye, you are logged out' });
+  }
 });
 
 module.exports = router;
